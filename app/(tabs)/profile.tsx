fb280a7b23ce2f732afd0be38534ac88d67503ae
Wagner Andrade <Wagnerandrade_df@hotmail.com>
2026-05-03 22:14:38 -0300

    feat(ui): add basic layout and navigation to library, search and profile screens

--- app/(tabs)/profile.tsx
+++ app/(tabs)/profile.tsx
@@ -1,5 +1,45 @@
-import ProfileScreen from "../../src/screens/ProfileScreen";
+import { router } from "expo-router";
+import { StyleSheet, Text, TouchableOpacity } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
 
-export default function Profile() {
-  return <ProfileScreen />;
+export default function LibraryScreen() {
+  return (
+    <SafeAreaView style={styles.container}>
+      <Text style={styles.title}>Profile</Text>
+
+      <TouchableOpacity style={styles.button} onPress={() => router.push("/")}>
+        <Text style={styles.buttonText}>Voltar para Home</Text>
+      </TouchableOpacity>
+    </SafeAreaView>
+  );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#0F172A",
+    justifyContent: "center",
+    alignItems: "center",
+    padding: 20,
+  },
+
+  title: {
+    color: "#E2E8F0",
+    fontSize: 24,
+    fontWeight: "bold",
+    marginBottom: 20,
+  },
+
+  button: {
+    backgroundColor: "#22C55E",
+    paddingVertical: 12,
+    paddingHorizontal: 30,
+    borderRadius: 10,
+  },
+
+  buttonText: {
+    color: "#000",
+    fontSize: 14,
+    fontWeight: "bold",
+  },
+});
